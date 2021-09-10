fbacd12f1645ced917a52566c45b56f543492f3a
rikicop <ruperto1@protonmail.com>
2021-09-10 14:52:02 -0400

    NavBar

---
+++ comps/MenuList.js
@@ -0,0 +1,14 @@
+export const MenuList = [
+    {
+        title:"Home",
+        url: "/"
+    },
+    {
+        title:"About",
+        url: "/about"
+    },
+    {
+        title:"Ninjas",
+        url: "/ninjas"
+    }
+]
--- comps/Navbar.js
+++ comps/Navbar.js
@@ -1,16 +1,42 @@
 
 import Link from 'next/link'
+import { useRouter } from 'next/router';
 import Image from 'next/image'
+import { FaBars } from 'react-icons/fa';
+import { FaTimes } from 'react-icons/fa';
+import { MenuList } from './MenuList';
+import { useState } from 'react';
+
+
 
 const Navbar = () => {
+    const [clicked, setClicked] = useState(false)
+    const router = useRouter();
+    const menuList = MenuList.map(({url,title}, index)=>{
+      return(
+        <li key={index}>
+          <Link href={url}>
+            <a className={router.pathname == url ? "active" : ""}>
+              {title}
+            </a>
+          </Link>
+        </li>
+      )
+    })
+    const handleClick = () =>{
+      setClicked(!clicked);
+    } 
     return (
         <nav>
              <div className="logo">
                <Image src="/logo.png"  width={128} height={77}/>
              </div>
-             <Link href='/' ><a>Home</a></Link>
-             <Link href='/about'><a>About</a></Link>
-             <Link href='/ninjas'><a>Ninja Listing</a></Link>
+             <div className="menu-icon" onClick={handleClick}>
+                { clicked ?  <FaTimes/> : <FaBars/>}              
+             </div>
+             <ul className={ clicked ? "menu-list": "menu-list close"} onClick={handleClick}>
+               {menuList}
+             </ul>
         </nav>
        
     )
